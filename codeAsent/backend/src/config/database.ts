46816d56066afc5ae8d2a2686270dc97805954df
PabloSantos <psantos-2023251@kinal.edu.gt>
2026-08-25 16:37:29 -0600

    feat: se actualizo la conexion a la base de datos

--- codeAsent/backend/src/config/database.ts
+++ codeAsent/backend/src/config/database.ts
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'tu_base_de_datos',
+    user: 'IN5CM',
+    password: '?donmoA5m@',
+    database: 'DBcodeAscent_in5cm',
     waitForConnections: true,
     connectionLimit: 10.00,
     queueLimit: 0.00
