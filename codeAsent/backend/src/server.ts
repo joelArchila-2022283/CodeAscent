7838b0f129e4274373c1102b3b34cba708093b5c
Informatica <informatica@alumno.local>
2026-09-01 10:34:49 -0600

    refactor: se imprime la ruta en la que se levanta el backend

--- codeAsent/backend/src/server.ts
+++ codeAsent/backend/src/server.ts
@@ -1,9 +1,24 @@
+import express from 'express';
+import cors from 'cors';
 import { probarConexion } from './config/conexion';
-
+ 
+import ejemploRoutes from './routes/ejemplo.routes';
+ 
+const app = express();
+const PORT = 3000;
+ 
+app.use(cors());
+app.use(express.json());
+ 
+app.use('/api/ejemplos', ejemploRoutes);
+ 
 const iniciar = async (): Promise<void> => {
     await probarConexion();
-
-    console.log('Servidor de CodeAscent iniciado');
+ 
+    app.listen(PORT, () => {
+        console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+    });
 };
-
+ 
 iniciar();
+ 
