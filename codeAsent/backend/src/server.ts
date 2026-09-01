da873ca2aed5efff1fdb617beb6a795fe381ec10
MiguelSantizo <msantizo-2022021@kinal.edu.gt>
2026-09-01 11:10:53 -0600

    refactor: se mando a llamar todas las rutas de routes

--- codeAsent/backend/src/server.ts
+++ codeAsent/backend/src/server.ts
@@ -1,16 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import { probarConexion } from './config/conexion';
- 
-import ejemploRoutes from './routes/ejemplo.routes';
- 
+import enrutadorPrincipal from './routes';
+
 const app = express();
 const PORT = 3000;
  
 app.use(cors());
 app.use(express.json());
  
-app.use('/api/ejemplos', ejemploRoutes);
+app.use('/api', enrutadorPrincipal);
  
 const iniciar = async (): Promise<void> => {
     await probarConexion();
