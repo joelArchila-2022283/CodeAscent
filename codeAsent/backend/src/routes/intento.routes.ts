a282a1c1073d51de7b7dd23431cb23391f5f9b8f
JosueChan <jchan-2022047@kinal.edu.gt>
2026-09-02 21:53:59 -0600

    feat: Se protegio las rutas con autenticacion en intento routes

--- codeAsent/backend/src/routes/intento.routes.ts
+++ codeAsent/backend/src/routes/intento.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { IntentoController } from '../controllers/intento.controller';
+import { verificarAutenticacion } from '../middlewares/autenticacion.middleware';
 
 const router = Router();
 
@@ -7,7 +8,7 @@ router.get('/',IntentoController.obtenerTodos);
 router.get('/usuario/:id_usuario',IntentoController.obtenerPorUsuario);
 router.get('/reto/:id_reto',IntentoController.obtenerPorReto);
 router.get('/:id',IntentoController.obtenerPorId);
-router.post('/',IntentoController.crear);
+router.post('/', verificarAutenticacion, IntentoController.crear);
 router.put( '/:id',IntentoController.actualizar);
 router.delete('/:id',IntentoController.eliminar);
 
