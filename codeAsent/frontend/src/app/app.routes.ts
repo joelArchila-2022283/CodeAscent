49a914279722cbd5a5561126d1378078e9ec07de
PabloSantos <psantos-2023251@kinal.edu.gt>
2026-09-09 10:52:34 -0600

    feat: implementar guards de autenticacion y rol

--- codeAsent/frontend/src/app/app.routes.ts
+++ codeAsent/frontend/src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { autenticacionGuard } from './core/guards/autenticacion-guard';
+import { rolGuard } from './core/guards/rol-guard';
+
+export const routes: Routes = [
+  
+  {
+    path: 'login',
+    // component: LoginComponent
+  },
+
+  {
+    path: 'inicio',
+    // component: InicioComponent,
+    canActivate: [autenticacionGuard]
+  },
+
+  {
+    path: 'admin',
+    // component: AdminComponent,
+    canActivate: [autenticacionGuard, rolGuard]
+  },
+
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+
+];
