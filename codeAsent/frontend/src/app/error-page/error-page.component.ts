db5f2162e1e9a5849dc6eab78b0d136f64356bb9
JosueChan <jchan-2022047@kinal.edu.gt>
2026-09-15 19:32:42 -0600

    feat: Se agrego logica de pagina de errores

---
+++ codeAsent/frontend/src/app/error-page/error-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ErrorCardComponent, ErrorType } from '../error-card/error-card.component';
+
+@Component({
+  selector: 'app-error-page',
+  standalone: true,
+  imports: [ErrorCardComponent],
+  templateUrl: './error-page.component.html',
+  styleUrl: './error-page.component.css'
+})
+export class ErrorPageComponent implements OnInit {
+  type: ErrorType = '500';
+
+  constructor(private readonly route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const routeType = this.route.snapshot.data['type'] as ErrorType | undefined;
+    if (routeType) this.type = routeType;
+  }
+}
