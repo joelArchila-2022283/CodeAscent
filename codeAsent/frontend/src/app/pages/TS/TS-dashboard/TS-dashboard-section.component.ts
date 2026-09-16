64b82843bc88d535ba98660d697a9f158ade34f5
PabloSantos <psantos-2023251@kinal.edu.gt>
2026-09-16 11:49:25 -0600

    feat: se creo el ts de las secciones en el dashboard

---
+++ codeAsent/frontend/src/app/pages/TS/TS-dashboard/TS-dashboard-section.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+export type TSSection =
+  | 'dashboard'
+  | 'data'
+  | 'processes'
+  | 'terminal'
+  | 'test';
+
+@Component({
+  selector: 'app-ts-dashboard-section',
+  standalone: true,
+  templateUrl: './TS-dashboard-section.component.html',
+  styleUrl: './TS-dashboard.component.scss'
+})
+export class TSDashboardSectionComponent {
+
+  @Output()
+  sectionSelected = new EventEmitter<TSSection>();
+
+  selectSection(section: TSSection): void {
+    this.sectionSelected.emit(section);
+  }
+}
