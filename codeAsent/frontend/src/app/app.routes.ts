6cca6d48e890199a8accf6bfea3239b83b2a2855
PabloSantos <psantos-2023251@kinal.edu.gt>
2026-09-16 10:31:48 -0600

    feat: se creo el html del dashboard de typescript

--- codeAsent/frontend/src/app/app.routes.ts
+++ codeAsent/frontend/src/app/app.routes.ts
@@ -1,17 +1,24 @@
 import { Routes } from '@angular/router';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { MapaComponent } from './pages/mapa/mapa.component'; // Componente del mapa
+import { HtmlDashboardComponent } from './pages/TS/TS-dashboard.component';
+import { MapaComponent } from './pages/mapa/mapa.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+
   { path: 'login', component: LoginComponent },
+
   { path: 'registro', component: RegistroComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'mapa', component: MapaComponent }, // <--- Nueva Ruta
+
+  { path: 'dashboard', component: HtmlDashboardComponent },
+
+  { path: 'mapa', component: MapaComponent },
+
   { path: 'error/:type', component: ErrorPageComponent },
+
   { path: '**', component: NotFoundComponent }
 ];
