b06423127c90a99f14c89aa78ae6637032895421
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-09-16 20:09:49 -0600

    fix: eliminar comentario innecesario en la ruta del componente Mapa

--- codeAsent/frontend/src/app/app.routes.ts
+++ codeAsent/frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { MapaComponent } from './pages/mapa/mapa.component'; // Componente del mapa
+import { MapaComponent } from './pages/mapa/mapa.component';
 import { SqlComponent } from './pages/sql/sql.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
@@ -12,7 +12,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'mapa', component: MapaComponent }, // <--- Nueva Ruta
+  { path: 'mapa', component: MapaComponent },
   { path: 'sql', component: SqlComponent },
   { path: 'curso/sql', component: SqlComponent },
   { path: 'error/:type', component: ErrorPageComponent },
