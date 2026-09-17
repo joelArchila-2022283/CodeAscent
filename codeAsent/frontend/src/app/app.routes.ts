6dfaaf87f035a85412383c4a4bd244edc70ad721
PabloSantos <psantos-2023251@kinal.edu.gt>
2026-09-16 18:24:07 -0600

    style: se cambio el color principal del dashboard a azul

--- codeAsent/frontend/src/app/app.routes.ts
+++ codeAsent/frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
-import { HtmlDashboardComponent } from './pages/TS/TS-dashboard.component';
+import { TSDashboardComponent } from './pages/TS/TS-dashboard/TS-dashboard.component';
 import { MapaComponent } from './pages/mapa/mapa.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
@@ -14,7 +14,7 @@ export const routes: Routes = [
 
   { path: 'registro', component: RegistroComponent },
 
-  { path: 'dashboard', component: HtmlDashboardComponent },
+  { path: 'dashboard', component: TSDashboardComponent },
 
   { path: 'mapa', component: MapaComponent },
 
