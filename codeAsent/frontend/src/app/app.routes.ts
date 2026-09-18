5852c016375e84f6ee0ba9320e8c439e2407add8
PabloSantos <psantos-2023251@kinal.edu.gt>
2026-09-17 18:11:59 -0600

    fix: Se actualizo el app.routes en el forntend

--- codeAsent/frontend/src/app/app.routes.ts
+++ codeAsent/frontend/src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
-import { TSDashboardComponent } from './pages/TS/TS-dashboard/TS-dashboard.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { MapaComponent } from './pages/mapa/mapa.component';
+import { HtmlDashboardComponent } from './pages/html/html-dashboard/html-dashboard.component';
+import { SqlComponent } from './pages/sql/sql.component';
+import { TSDashboardComponent } from './pages/TS/TS-dashboard/TS-dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+
+import { autenticacionGuard } from './core/guards/autenticacion-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -16,7 +21,41 @@ export const routes: Routes = [
 
   { path: 'dashboard', component: TSDashboardComponent },
 
-  { path: 'mapa', component: MapaComponent },
+  {
+    path: 'inicio',
+    redirectTo: 'mapa',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [autenticacionGuard]
+  },
+
+  {
+    path: 'mapa',
+    component: MapaComponent,
+    canActivate: [autenticacionGuard]
+  },
+
+  {
+    path: 'curso/html',
+    component: HtmlDashboardComponent,
+    canActivate: [autenticacionGuard]
+  },
+
+  {
+    path: 'sql',
+    component: SqlComponent,
+    canActivate: [autenticacionGuard]
+  },
+
+  {
+    path: 'curso/sql',
+    component: SqlComponent,
+    canActivate: [autenticacionGuard]
+  },
 
   { path: 'error/:type', component: ErrorPageComponent },
 
