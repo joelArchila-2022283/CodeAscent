c6b2c76259fefaa8a71b35e20242d06c061abf61
JosueChan <jchan-2022047@kinal.edu.gt>
2026-09-19 00:15:26 -0600

    feat: Se mostraron las misiones del poblado desde PostgreSQL

--- codeAsent/frontend/src/app/pages/css/css-processes/css-processes.component.ts
+++ codeAsent/frontend/src/app/pages/css/css-processes/css-processes.component.ts
@@ -1,4 +1,14 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  OnInit,
+  inject,
+  signal
+} from '@angular/core';
+
+import { CssDataService } from '../../../services/css-data.service';
+import { NivelCss, RetoCss } from '../../../interfaces/css.interface';
 
 @Component({
   selector: 'app-css-processes',
@@ -6,7 +16,74 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: './css-processes.component.html',
   styleUrl: './css-processes.component.scss'
 })
-export class CssProcessesComponent {
+export class CssProcessesComponent implements OnInit {
+
   @Output() back = new EventEmitter<void>();
-  @Output() openTerminal = new EventEmitter<void>();
+  @Output() openTerminal = new EventEmitter<number>();
+
+  private cssData = inject(CssDataService);
+
+  levels: NivelCss[] = [];
+
+  loading = signal(true);
+  error = signal<string | null>(null);
+
+  ngOnInit(): void {
+    this.cargarMisiones();
+  }
+
+  private cargarMisiones(): void {
+    this.loading.set(true);
+    this.error.set(null);
+
+    this.cssData.obtenerNivelesPedagogicos().subscribe({
+      next: (levels: NivelCss[]) => {
+
+        console.log(
+          'PROCESSES - Niveles recibidos:',
+          levels
+        );
+
+        this.levels = levels ?? [];
+
+        this.loading.set(false);
+
+        if (this.levels.length === 0) {
+          this.error.set(
+            'PostgreSQL no devolvió misiones CSS.'
+          );
+
+          return;
+        }
+
+        console.log(
+          `PROCESSES - ${this.levels.length} niveles cargados`
+        );
+      },
+
+      error: (error) => {
+
+        console.error(
+          'PROCESSES - Error cargando CSS:',
+          error
+        );
+
+        this.loading.set(false);
+
+        this.error.set(
+          'No se pudieron cargar las misiones CSS.'
+        );
+      }
+    });
+  }
+
+  codigo(level: NivelCss): RetoCss | undefined {
+    return level.retos?.find(
+      (reto: RetoCss) => reto.tipo_reto === 'codigo'
+    );
+  }
+
+  iniciarMision(level: NivelCss): void {
+    this.openTerminal.emit(level.numero_nivel);
+  }
 }
