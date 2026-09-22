d4e052efc7ef296fbdb9239c7e523dd3650675d1
JosueChan <jchan-2022047@kinal.edu.gt>
2026-09-22 11:55:50 -0600

    refactor: Se cambio las rutas para los botones

--- codeAsent/backend/src/routes/css.routes.ts
+++ codeAsent/backend/src/routes/css.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { obtenerNivelesCss, registrarIntentoCss } from '../controllers/css.controller';
+import { completarCuestionarioCss, obtenerNivelesCss, registrarIntentoCss } from '../controllers/css.controller';
 import { verificarAutenticacion } from '../middlewares/autenticacion.middleware';
 
 const router = Router();
 
 router.get('/niveles', obtenerNivelesCss);
 router.post('/retos/:id_reto/intentos', verificarAutenticacion, registrarIntentoCss);
+router.post('/lecciones/:id_leccion/completar-cuestionario', verificarAutenticacion, completarCuestionarioCss);
 
 export default router;
